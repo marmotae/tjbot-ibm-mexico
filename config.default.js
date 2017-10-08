6b1ba82ef93d0686e98734d2bfbf4793e1af979e
marmotae <32424320+marmotae@users.noreply.github.com>
2017-10-08 13:34:52 -0500

    Actualizacion del config.default.js

--- config.default.js
+++ config.default.js
@@ -1,29 +1,52 @@
-/**
- * Ejemplo de credenciales de servicio
- */
+/*
+ * Configuración de los parametros generales
+ */ 
 
-var credentials = {
-	conversation: {
-		username: 'xxx',
-		password: 'yyy'
-	},
-	language_translator: {
-		username: 'xxx',
-		password: 'yyy'
-	},
-	speech_to_text: {
-		username: 'xxx',
-		password: 'yyy'
-	},
-	text_to_speech: {
-		username: 'xxx',
-		password: 'yyy'
-	},
-	tone_analyzer: {
-		username: 'xxx',
-		password: 'yyy'
-	},
-	visual_recognition: {
-		api_key: 'xxx'
-	}
+// Configuravion de nivel de bitacoreo
+exports.loglevel = 'info';
+
+// Configuración para workspaceId de conversation
+exports.conversationWorkspaceId = ''; 
+
+// Configuración para twitter
+exports.twitterConfig = {
+		consumer_key:         '',
+		consumer_secret:      '',
+		access_token:         '',
+		access_token_secret:  '',
+		timeout_ms:            60*1000
+		}
+
+// Huso horario para la conversacion
+// Ver en https://console.bluemix.net/docs/services/conversation/supported-timezones.html#time-zones-supported-by-system-entities
+exports.timezone = 'Mexico/General';
+
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Conversation
+// https://www.ibm.com/watson/developercloud/conversation.html
+exports.credentials.conversation = {
+	password: '',
+	username: ''
+};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+	password: '',
+	username: ''
+};
+
+// Watson Text to Speech
+// https://www.ibm.com/watson/developercloud/text-to-speech.html
+exports.credentials.text_to_speech = {
+	password: '',
+	username: ''
+};
+
+// Watson Visual Recognition
+// https://gateway-a.watsonplatform.net/visual-recognition/api
+exports.credentials.visual_recognition = {
+	api_key: "",
 };
